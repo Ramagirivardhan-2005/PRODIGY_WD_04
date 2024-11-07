61d265370457644b3756f5b4d53ccef5c7e6782c
Ramagirivardhan-2005 <vardhanramagiri84@gmail.com>
2024-11-07 11:15:04 +0530

    Add files via upload

---
+++ Hr4.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('a[href^="vardhanramagiri84@gmail.com"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
